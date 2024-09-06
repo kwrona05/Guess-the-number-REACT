d38bb229df3d10ec3ee840df756f9a8de9fac948
kwrona05 <kwrona05@gmail.com>
2024-09-06 08:58:12 +0200

    Create concept guess the number app

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
   return(
-    <div className="container"></div>
+    <div className="container">
+      <header>
+        <h1>Gues the number</h1>
+        <p>Can you beat the computer?</p>
+      </header>
+      <menu className="buttons">
+        <button className="easy">Easy</button>
+        <button className="medium">Medium</button>
+        <button className="hard">Hard</button>
+      </menu>
+      <div className="game-container">
+        <strong className="try">5</strong>
+        <input className="user-number" placeholder="Enter number"></input>
+        <p className="result-text"></p>
+      </div>
+    </div>
   )
 }
+export default App
